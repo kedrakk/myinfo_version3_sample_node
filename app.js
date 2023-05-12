6c66055122ee0dcf788d356a40e78288c9cb4f17
kedrakk <keaindrak@gmail.com>
2023-05-12 17:29:54 +0630

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config/config.js');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const cors = require('cors');
+const crypto = require('crypto');
+var MyInfoConnector = require('myinfo-connector-nodejs');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.set('views', path.join(__dirname, 'front'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.resolve(__dirname, 'front')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"front",'index.html'));
+// });
+app.get('/callback', function (req, res) {
+    res.sendFile(__dirname + '/front/index.html');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + 'front/index.html');
+});
+
+
+app.get('/getEnv', function (req, res) {
+
+    try {
+        if (config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined || config.APP_CONFIG.DEMO_APP_CLIENT_ID == null) {
+            res.status(500).send({
+                "error": "Missing Client ID"
+            });
+        } else {
+            res.status(200).send({
+                "clientId": config.APP_CONFIG.DEMO_APP_CLIENT_ID,
+                "redirectUrl": config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
+                "attributes": config.APP_CONFIG.DEMO_APP_SCOPES,
+                "purpose": config.APP_CONFIG.DEMO_APP_PURPOSE,
+                "environment": "TEST",
+                "authApiUrl": config.APP_CONFIG.MYINFO_API_AUTHORISE["TEST"],
+            });
+        }
+    } catch (error) {
+        console.log("Error".red, error);
+        res.status(500).send({
+            "error": error
+        });
+    }
+});
+
+app.post('/getPersonData', function (req, res, next) {
+
+    try {
+      var authCode = req.body.authCode;
+      var state = req.body.state;
+      var txnNo = crypto.randomBytes(10).toString("hex");
+  
+      let connector = new MyInfoConnector(config.MYINFO_CONNECTOR_CONFIG);
+      console.log("Calling MyInfo NodeJs Library...".green);
+  
+      connector.getMyInfoPersonData(authCode, state, txnNo)
+        .then(personData => {
+          res.status(200).send(personData); //return personData
+        })
+        .catch(error => {
+          res.status(500).send({
+            "error": error
+          });
+        });
+    } catch (error) {
+      console.log("Error".red, error);
+      res.status(500).send({
+        "error": error
+      });
+    }
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
