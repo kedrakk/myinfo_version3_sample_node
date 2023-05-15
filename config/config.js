bf414e9d609a43a7c9c1325c8de7e3cbcde38320
kedrakk <keaindrak@gmail.com>
2023-05-15 11:01:43 +0630

    added gitignore

--- config/config.js
+++ config/config.js
@@ -9,33 +9,16 @@ let APP_CONFIG = {
   'MYINFO_API_AUTHORISE': 'https://test.api.myinfo.gov.sg/com/v3/authorise'
 }
 
-
-//Set following configuration for MyInfo library to call token and person API
-// IMPORTANT: DO NOT rename the JSON Keys
 let MYINFO_CONNECTOR_CONFIG = {
   'MYINFO_SIGNATURE_CERT_PUBLIC_CERT': './cert/je-public-cert.pem',
 
-  'CLIENT_ID': APP_CONFIG.DEMO_APP_CLIENT_ID, //Client id provided during onboarding
-  'CLIENT_SECRET': APP_CONFIG.DEMO_APP_CLIENT_SECRET, //Client secret provided during onboarding
-  'CLIENT_SECURE_CERT': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT, //Alias of the application private key in P12 format.
-  'CLIENT_SECURE_CERT_PASSPHRASE': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT_PASSPHRASE, //Password of the private key.
-  'REDIRECT_URL': APP_CONFIG.DEMO_APP_CALLBACK_URL, //Redirect URL for web application
+  'CLIENT_ID': APP_CONFIG.DEMO_APP_CLIENT_ID, 
+  'CLIENT_SECRET': APP_CONFIG.DEMO_APP_CLIENT_SECRET,
+  'CLIENT_SECURE_CERT': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT, 
+  'CLIENT_SECURE_CERT_PASSPHRASE': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT_PASSPHRASE, 
+  'REDIRECT_URL': APP_CONFIG.DEMO_APP_CALLBACK_URL, 
   'ATTRIBUTES': APP_CONFIG.DEMO_APP_SCOPES,
-
-
-  /* 
-  Without Encryption and Signing 
-  Note: The sandbox environment is used for your testing when developing your prototype
-  */
-  // 'ENVIRONMENT': 'SANDBOX',
-  // 'TOKEN_URL': 'https://sandbox.api.myinfo.gov.sg/com/v3/token',
-  // 'PERSON_URL': 'https://sandbox.api.myinfo.gov.sg/com/v3/person',
-
-
-  /* 
-  With Encryption and Signing 
-  Note: The test environment is used for testing your application with the full security measures required in production
-  */
+ 
   'ENVIRONMENT': 'TEST',
   'TOKEN_URL': 'https://test.api.myinfo.gov.sg/com/v3/token',
   'PERSON_URL': 'https://test.api.myinfo.gov.sg/com/v3/person',
@@ -46,14 +29,6 @@ let MYINFO_CONNECTOR_CONFIG = {
   'PROXY_TOKEN_URL': '', //Configure your proxy url here, if any.
   'PROXY_PERSON_URL': '', //Configure your proxy url here, if any.
 
-  /*
-  Debug level for library logging. i.e 'error, info, debug' leave empty to turn off logs (OPTIONAL)
-   * error - Log out all the errors returned from the library
-   * info - log urls called, authorization headers and errors from the library
-   * debug - Full logs from the library, i.e (errors, urls, authorization headers, API response) 
-  
-  NOTE: debug mode should never be turned on in production
-  */
   'DEBUG_LEVEL': 'info'
 }
 
