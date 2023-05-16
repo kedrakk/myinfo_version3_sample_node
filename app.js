2afc082dc6b052d4b1a2d801753ebc5a9a2c0e37
kedrakk <keaindrak@gmail.com>
2023-05-16 11:15:18 +0630

    added .env

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const config = require('./config/config.js');
@@ -21,7 +23,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(cookieParser());
 
@@ -30,60 +32,62 @@ app.get('/callback', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/view/index.html');
+  res.sendFile(__dirname + '/public/view/index.html');
 });
 
 
 app.get('/getEnv', function (req, res) {
-    try {
-        if (config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined || config.APP_CONFIG.DEMO_APP_CLIENT_ID == null) {
-            res.status(500).send({
-                "error": "Missing Client ID"
-            });
-        } else {
-            res.status(200).send({
-                "clientId": config.APP_CONFIG.DEMO_APP_CLIENT_ID,
-                "redirectUrl": config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
-                "attributes": config.APP_CONFIG.DEMO_APP_SCOPES,
-                "purpose": config.APP_CONFIG.DEMO_APP_PURPOSE,
-                "authApiUrl": config.APP_CONFIG.MYINFO_API_AUTHORISE,
-            });
-        }
-    } catch (error) {
-        console.log("Error".red, error);
-        res.status(500).send({
-            "error": error
-        });
+  try {
+    if (process.env.DEMO_APP_CLIENT_ID == undefined || process.env.DEMO_APP_CLIENT_ID == null) {
+      res.status(500).send({
+        "error": "Missing Client ID"
+      });
+    } else {
+      res.status(200).send({
+        "clientId": process.env.DEMO_APP_CLIENT_ID,
+        "redirectUrl": config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
+        "attributes": config.APP_CONFIG.DEMO_APP_SCOPES,
+        "purpose": config.APP_CONFIG.DEMO_APP_PURPOSE,
+        "authApiUrl": config.APP_CONFIG.MYINFO_API_AUTHORISE,
+      });
     }
+  } catch (error) {
+    console.log("Error".red, error);
+    res.status(500).send({
+      "error": error
+    });
+  }
 });
 
 app.post('/getPersonData', function (req, res, next) {
-  
-    try {
-      var authCode = req.body.authCode;
-      var state = req.body.state;
-      var txnNo = crypto.randomBytes(10).toString("hex");
-      
-      let connector = new MyInfoConnector(config.MYINFO_CONNECTOR_CONFIG);
-      console.log("Calling MyInfo NodeJs Library...".green);
-  
-      connector.getMyInfoPersonData(authCode, state, txnNo)
-        .then(personData => {
-          res.status(200).send(personData); //return personData
-        })
-        .catch(error => {
-          res.status(500).send({
-            "error": error
-          });
-        });
-    } catch (error) {
-      console.log("Error".red, error);
-      res.status(500).send({
-        "error": error
-      });
-    }
-  });
 
-app.listen(PORT,() => {
-    console.log(`Server listening on ${PORT}`);
+  try {
+    var authCode = req.body.authCode;
+    var state = req.body.state;
+    var txnNo = crypto.randomBytes(10).toString("hex");
+
+    var myinfoConfig = config.MYINFO_CONNECTOR_CONFIG(process.env.DEMO_APP_CLIENT_ID,process.env.DEMO_APP_CLIENT_SECRET);
+
+    let connector = new MyInfoConnector(myinfoConfig);
+    console.log("Calling MyInfo NodeJs Library...".green);
+
+    connector.getMyInfoPersonData(authCode, state, txnNo)
+      .then(personData => {
+        res.status(200).send(personData); //return personData
+      })
+      .catch(error => {
+        res.status(500).send({
+          "error": error
+        });
+      });
+  } catch (error) {
+    console.log("Error".red, error);
+    res.status(500).send({
+      "error": error
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 })
--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
 let APP_CONFIG = {
-  'DEMO_APP_CLIENT_ID': 'STG2-MYINFO-DEMO-APP',
-  'DEMO_APP_CLIENT_SECRET': 'outzuu7n3bxzcvdyrv98y3picshnkydf1r4ybwas',
   'DEMO_APP_CLIENT_SECURE_CERT': './cert/sample-private-cert.p12',
   'DEMO_APP_CLIENT_SECURE_CERT_PASSPHRASE': 'DemoApp',
   'MYINFO_SIGNATURE_CERT_PUBLIC_CERT': './cert/sample-public-cert.pem',
@@ -10,27 +8,21 @@ let APP_CONFIG = {
   'MYINFO_API_AUTHORISE': 'https://test.api.myinfo.gov.sg/com/v3/authorise'
 }
 
-let MYINFO_CONNECTOR_CONFIG = {
-  'MYINFO_SIGNATURE_CERT_PUBLIC_CERT': APP_CONFIG.MYINFO_SIGNATURE_CERT_PUBLIC_CERT,
-  'CLIENT_ID': APP_CONFIG.DEMO_APP_CLIENT_ID,
-  'CLIENT_SECRET': APP_CONFIG.DEMO_APP_CLIENT_SECRET,
-  'CLIENT_SECURE_CERT': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT,
-  'CLIENT_SECURE_CERT_PASSPHRASE': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT_PASSPHRASE,
-  'REDIRECT_URL': APP_CONFIG.DEMO_APP_CALLBACK_URL,
-  'ATTRIBUTES': APP_CONFIG.DEMO_APP_SCOPES,
-
-  'ENVIRONMENT': 'TEST',
-  'TOKEN_URL': 'https://test.api.myinfo.gov.sg/com/v3/token',
-  'PERSON_URL': 'https://test.api.myinfo.gov.sg/com/v3/person',
-
-
-  //Proxy parameters (OPTIONAL) 
-  'USE_PROXY': 'N', //Indicate whether proxy url is used. i.e. Y or N
-  'PROXY_TOKEN_URL': '', //Configure your proxy url here, if any.
-  'PROXY_PERSON_URL': '', //Configure your proxy url here, if any.
-
-  'DEBUG_LEVEL': 'info'
-}
-
 module.exports.APP_CONFIG = APP_CONFIG;
-module.exports.MYINFO_CONNECTOR_CONFIG = MYINFO_CONNECTOR_CONFIG;
+module.exports.MYINFO_CONNECTOR_CONFIG = function (clientId,secretKey) {
+  return {
+    'MYINFO_SIGNATURE_CERT_PUBLIC_CERT': APP_CONFIG.MYINFO_SIGNATURE_CERT_PUBLIC_CERT,
+    'CLIENT_ID': clientId,
+    'CLIENT_SECRET': secretKey,
+    'CLIENT_SECURE_CERT': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT,
+    'CLIENT_SECURE_CERT_PASSPHRASE': APP_CONFIG.DEMO_APP_CLIENT_SECURE_CERT_PASSPHRASE,
+    'REDIRECT_URL': APP_CONFIG.DEMO_APP_CALLBACK_URL,
+    'ATTRIBUTES': APP_CONFIG.DEMO_APP_SCOPES,
+  
+    'ENVIRONMENT': 'TEST',
+    'TOKEN_URL': 'https://test.api.myinfo.gov.sg/com/v3/token',
+    'PERSON_URL': 'https://test.api.myinfo.gov.sg/com/v3/person',
+  
+    'DEBUG_LEVEL': 'info'
+  }
+};
